38f488361b636023c8bc388fa1795182066a0854
VallanTV <thevallantv@protonmail.com>
2020-05-19 19:53:15 -0700

    Add Russian translation, bump version
    Thanks https://github.com/Hubbotu

--- Locales/ruRU.lua
+++ Locales/ruRU.lua
@@ -3,4 +3,5 @@ local L = LibStub("AceLocale-3.0"):NewLocale(ADDON_NAME, "ruRU")
 if not L then return end
 
 
+L["Automatically turns on the combat log for selected raids."] = "Автоматически включается журнал боя для выбранных рейдов"
 L["Profiles"] = "Профили"
